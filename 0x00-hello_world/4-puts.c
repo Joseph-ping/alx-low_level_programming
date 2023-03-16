cf6715529ea2f8311d595606153876dac7806e7b
Joseph-ping <ngunjirijoseph412@gmail.com>
2023-03-16 08:06:08 -0700

    C-Programming with Betty

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+*put the main in the put string
+*using the main function
+*this is c file 
+*c programming
+*this program prints "Programming is like building a multilingual puzzle
+*Return: 0
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	Return (0);
+}
