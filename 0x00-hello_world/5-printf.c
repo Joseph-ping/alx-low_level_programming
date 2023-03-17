07172de735baf91e2c52bf0d64a63a08d9c8f385
Joseph-ping <ngunjirijoseph412@gmail.com>
2023-03-17 06:24:46 -0700

    C-Programming with Betty

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 /**
-*main- put the function string as to print the line
+* main - print the string in the put function
 *
-*Description: using the main funstion
-*this program prints with with proper grammar, but the outcome is a piece of art,
-*Return: 0
+* Description: using the main function
+* this program prints "Programming is like building a multilingual puzzle
+* Return: 0
 */
-
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
