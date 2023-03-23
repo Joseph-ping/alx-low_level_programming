4703ef08aac25a70c6ec33a5a9cde6eea8f561bb
Joseph-ping <ngunjirijoseph412@gmail.com>
2023-03-23 09:31:56 -0700

    A continued move to more functions nested loops

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,13 +2,15 @@
 #include "main.h"
 
 /**
- * more_numbers - prints numbers 0 to 14
- *in 10 rows
+ * more_numbers - prints numbers to 14
+ *
  * Return: ALways 0 
  */
+
 void more_numbers(void)
 {
 	int n1, n2;
+	
 	for (n1 = 0; n1 < 10; n1++)
 	{
 		for (n2 = 0; n2 <= 14; n2++)
@@ -17,9 +19,8 @@ void more_numbers(void)
 			{
 				_putchar((n2 / 10) + '0');
 			}
-		       		_putchar((n2 % 10) + '0');
+			_putchar((n2 % 10) + '0');
 		}
-				_putchar(10);
+		_putchar(10);
 	}
 }
-
