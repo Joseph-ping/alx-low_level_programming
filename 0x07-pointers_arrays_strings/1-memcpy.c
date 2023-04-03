751ac4643d46bb131312d2dcaa8ee5c31510d054
Joseph-ping <ngunjirijoseph412@gmail.com>
2023-04-03 07:07:34 -0700

    Pointers pointing on pointers becoming pointer pointers

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,21 +1,21 @@
 #include "main.h"
 
 /**
- * _memcpy - Copies @q bytes from the memory area pointed
+ * _memcpy - Copies @n bytes from the memory area pointed
  * to by @src into that pointed to by @dest.
- * @dest: Copy @src into this pointer
- * @src: Copy characters from this source buffer.
- * @q: The number of bytes to copy from @src
+ * @dest: A pointer to the memory area to copy @src into.
+ * @src: The source buffer to copy characters from.
+ * @n: The number of bytes to copy from @src
  *
  * Return: A pointer to the destination buffer @dest.
  */
-char *_memcpy(char *dest, char *src, unsigned int q)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
+	unsigned int index;
+	char *destination = dest;
 	char *source = src;
-    char *destination = dest;
-    unsigned int index;
-	
-	for (index = 0; index < q; index++)
+
+	for (index = 0; index < n; index++)
 		destination[index] = source[index];
 
 	return (dest);
