9b873dae5f2bcf380cb1e3860a49c09bc75ca557
Joseph-ping <ngunjirijoseph412@gmail.com>
2023-04-03 06:37:43 -0700

    Pointers pointing on pointers becoming pointer pointers

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,24 +1,24 @@
 #include "main.h"
 
 /**
- * _strpbrk - Searches a string for any  set of bytes.
- * @q: The string to be searched.
+ * _strpbrk - Searches a string for any of a set of bytes.
+ * @s: The string to be searched.
  * @accept: The set of butes to be searched for.
  *
  * Return: If a set is matched - a pointer to be matched byte.
  * If no set is matched - NULL.
  */
-char *_strpbrk(char *q, char *accept)
+char *_strpbrk(char *s, char *accept)
 {
 	int index;
 
-	while (*q)
+	while (*s)
 	{
 		for (index = 0; accept[index]; index++)
 		{
-			if (*q == accept[index])
-				return (q);
+			if (*s == accept[index])
+				return (s);
 		}
 
-		q++;
+		s++;
 	}
