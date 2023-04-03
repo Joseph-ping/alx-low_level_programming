0836f816cacd9d82fd18a58590f6ba53b770611e
Joseph-ping <ngunjirijoseph412@gmail.com>
2023-04-03 06:56:43 -0700

    Pointers pointing on pointers becoming pointer pointers

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * _strspn - Records the length of a prefix substring.
- * @s: The searched string .
- * @accept: The prefix under measure.
+ * _strspn - Gets the length of a prefix substring.
+ * @accept: The prefix to be measured.
+ * @s: The string to be searched.
  *
  * Return: The number of bytes in s which
  * consist only of bytes from accept
@@ -22,3 +22,13 @@ unsigned int _strspn(char *s, char *accept)
 				bytes++;
 				break;
 			}
+
+			else if (accept[index + 1] == '\0')
+				return (bytes);
+		}
+
+		s++;
+	}
+
+	return (bytes);
+}
