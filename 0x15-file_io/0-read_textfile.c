155bf078a82b453b6af5c2f36842ca0a39645cc4
Joseph-ping <ngunjirijoseph412@gmail.com>
2023-05-23 05:41:21 -0700

    File I/O tasks

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,28 +1,31 @@
 #include "main.h"
 #include <stdlib.h>
 
+
 /**
-*read_textfile- Read text file and print to STDOUT.
-*@filename: textfile being read
-*@letters: number of letters to be read
-*Return: W- actual number of bytes read and printed
-*	0 when function fails or filename is NULL
-*/
+ * read_textfile- Read text file print to STDOUT.
+ * @filename: text file being read
+ * @letters: number of letters to be read
+ * Return: w- actual number of bytes read and printed
+ *        0 when filename is NULL or  function fails .
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *buf;
-	ssize_t fd;
-	ssize_t W;
-	ssize_t t;
+        char *buf;
+        ssize_t fd;
+        ssize_t w;
+        ssize_t t;
 
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-		return (0);
-	buf = malloc(sizeof(char) * letters);
-	t = read(fd, buf, letters);
-	W = Write(STDOUT_FILENO, buf, t);
 
-	free(buf);
-	close(fd);
-	return(W);
+        fd = open(filename, O_RDONLY);
+        if (fd == -1)
+                return (0);
+        buf = malloc(sizeof(char) * letters);
+        t = read(fd, buf, letters);
+        w = write(STDOUT_FILENO, buf, t);
+
+
+        free(buf);
+        close(fd);
+        return (w);
 }
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,39 +1,41 @@
 #include "main.h"
 
+
 /**
- * append_text_to_file - appends text at the end of a file
- * @filename: filename.
- * @text_content: added content.
+ * append_text_to_file - Appends text at the end of a file.
+ * @filename: A pointer to the name of the file.
+ * @text_content: The string to add to the end of the file.
  *
- * Return: 1 if the file exists. -1 if the fails does not exist
- * or if it fails.
+ * Return: If the function fails or filename is NULL - -1.
+ *         If the file does not exist the user lacks write permissions - -1.
+ *         Otherwise - 1.
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd;
-	int nletters;
-	int rwr;
+        int o, w, len = 0;
 
-	if (!filename)
-		return (-1);
 
-	fd = open(filename, O_WRONLY | O_APPEND);
+        if (filename == NULL)
+                return (-1);
 
-	if (fd == -1)
-		return (-1);
 
-	if (text_content)
-	{
-		for (nletters = 0; text_content[nletters]; nletters++)
-			;
+        if (text_content != NULL)
+        {
+                for (len = 0; text_content[len];)
+                        len++;
+        }
 
-		rwr = write(fd, text_content, nletters);
 
-		if (rwr == -1)
-			return (-1);
-	}
+        o = open(filename, O_WRONLY | O_APPEND);
+        w = write(o, text_content, len);
 
-	close(fd);
 
-	return (1);
+        if (o == -1 || w == -1)
+                return (-1);
+
+
+        close(o);
+
+
+        return (1);
 }
